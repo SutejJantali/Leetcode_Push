cc6e08e2f01038594cb09f175d7f0ba5b74b2b39
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2025-03-31 02:28:04 +0530

    [1903] [Time Beats: 69.83%] [Memory Beats: 21.45%] - LeetPush

---
+++ Largest-Odd-Number-in-String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        res = ""
+        for i in range(len(num)-1, -1, -1):
+            if int(num[i]) % 2 == 1:
+                res += num[:i+1]
+                return res            
+        
+        return res
