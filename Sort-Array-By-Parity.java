70731fa075de33d06d1be3a66c938c9fd47b54cf
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-02-04 01:48:21 +0530

    [905] [Time Beats: 100.00%] [Memory Beats: 12.10%] - LeetPush

--- Sort-Array-By-Parity.java
+++ Sort-Array-By-Parity.java
@@ -1,24 +1,21 @@
 1class Solution {
 2    public int[] sortArrayByParity(int[] nums) {
 3        int i = 0, j = nums.length-1;
-4        if (j == 0){
-5            return nums;
-6        }
-7        while (i < j){
-8            while (i < j && nums[i] % 2 == 0){
-9                i+=1;
+4        while (i < j){
+5            while (i < j && nums[i] % 2 == 0){
+6                i+=1;
+7            }
+8            while (i < j && nums[j] % 2 == 1){
+9                j -=1;
 10            }
-11            while (i < j && nums[j] % 2 == 1){
-12                j -=1;
-13            }
-14            if (nums[i] % 2 == 1 && nums[j] % 2 == 0){
-15                int temp = nums[i];
-16                nums[i] = nums[j];
-17                nums[j] = temp;
-18                i += 1;
-19                j -= 1;
-20            }
-21        }
-22        return nums;
-23    }
-24}
+11            if (nums[i] % 2 == 1 && nums[j] % 2 == 0){
+12                int temp = nums[i];
+13                nums[i] = nums[j];
+14                nums[j] = temp;
+15                i += 1;
+16                j -= 1;
+17            }
+18        }
+19        return nums;
+20    }
+21}
