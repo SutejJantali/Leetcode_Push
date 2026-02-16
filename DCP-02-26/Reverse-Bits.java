b9afc148abee12f68488d2f3517340ccf222bfdb
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-02-16 20:37:34 +0530

    [190] [Time Beats: 60.34%] [Memory Beats: 96.57%] - LeetPush

---
+++ DCP-02-26/Reverse-Bits.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public int reverseBits(int n) {
+3        int res = 0;
+4        for (int i = 0; i < 32; i++){
+5            res <<= 1;
+6            res |= (n&1);
+7            n >>= 1;
+8        }
+9
+10        return res;
+11    }
+12}
