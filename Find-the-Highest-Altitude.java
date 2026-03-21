8dd7b412a21b83eaf7699a69009707b1f18c81fd
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-03-21 23:11:53 +0530

    [1732] [Time Beats: 100.00%] [Memory Beats: 69.54%] - LeetPush

--- Find-the-Highest-Altitude.java
+++ Find-the-Highest-Altitude.java
@@ -1,17 +1,16 @@
 1class Solution {
 2    public int largestAltitude(int[] gain) {
-3        int[] res_arr = new int[gain.length+1];
-4        res_arr[0] = 0;
-5        res_arr[1] = gain[0];
-6        int res = gain[0];
-7        for (int i = 1; i < gain.length; i++){
-8            res_arr[i+1] = gain[i] + res_arr[i];
-9            res = Math.max(res, res_arr[i+1]);
-10        }
-11
-12        if (res < 0)
-13        return 0;
-14        
-15        return res;
-16    }
-17}
+3        int res = gain[0];
+4        int curr = gain[0];
+5        for (int i = 1; i < gain.length; i++){
+6            int nxt = curr + gain[i];
+7            res = Math.max(res, nxt);
+8            curr = nxt;
+9        }
+10
+11        if (res < 0)
+12        return 0;
+13
+14        return res;
+15    }
+16}
