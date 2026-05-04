6b095d00d8b3cad2c815e638b7389cb89a43f80f
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-05-05 02:10:31 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 5.62%] - LeetPush

---
+++ DCP-05-26/Rotate-Image.py
@@ -0,0 +1,12 @@
+1class Solution:
+2    def rotate(self, matrix: List[List[int]]) -> None:
+3        """
+4        Do not return anything, modify matrix in-place instead.
+5        """
+6        for i in range(len(matrix)):
+7            for j in range(i+1, len(matrix)):
+8                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+9
+10
+11        for row in matrix:
+12            row.reverse()     
