45e1dd24f6f8f1729cb56243112e6191d3e07bb1
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-06-22 07:30:27 +0530

    [1189] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- DCP-06-26/Maximum-Number-of-Balloons.java
+++ DCP-06-26/Maximum-Number-of-Balloons.java
@@ -1,37 +1,48 @@
 1class Solution {
 2    public int maxNumberOfBalloons(String text) {
-3        String test = "abllnoo";
-4        StringBuilder temp = new StringBuilder();
-5
-6        for (int i = 0; i < text.length(); i++){
-7            if (test.indexOf(text.charAt(i)) > -1)
-8            temp.append(text.charAt(i));
-9        }
-10        char[] arr = temp.toString().toCharArray();
-11        Arrays.sort(arr);
-12        StringBuilder remaining = new StringBuilder(new String(arr));
-13
-14        int res = 0;
-15        while (true){
-16            boolean found = true;
-17
-18            for (char c : test.toCharArray()){
-19                int idx = remaining.indexOf(String.valueOf(c));
-20                if (idx == -1){
-21                    found = false;
-22                    break;
-23                }
-24                
-25
-26                remaining.deleteCharAt(idx);
-27            }
-28
-29            if (!found)
-30            break;
-31
-32            res += 1;
-33        }
-34
-35        return res;
-36    }
-37}
+3        int[] count = new int[26];
+4
+5        for (char c : text.toCharArray()) {
+6            count[c - 'a']++;
+7        }
+8
+9        return Math.min(
+10                Math.min(count['b' - 'a'], count['a' - 'a']),
+11                Math.min(
+12                    Math.min(count['l' - 'a'] / 2, count['o' - 'a'] / 2),
+13                    count['n' - 'a']
+14                )
+15        );
+16    }
+17}
+18
+19/*min(
+20    count(b),
+21    count(a),
+22    count(l)/2,
+23    count(o)/2,
+24    count(n)
+25)
+26text = "loonbalxballpoon"
+27b = 2
+28a = 2
+29l = 4
+30o = 4
+31n = 2
+32b -> 2/1 = 2 balloons
+33a -> 2/1 = 2 balloons
+34l -> 4/2 = 2 balloons
+35o -> 4/2 = 2 balloons
+36n -> 2/1 = 2 balloons
+37
+38int b = count['b' - 'a'];
+39int a = count['a' - 'a'];
+40int l = count['l' - 'a'] / 2;
+41int o = count['o' - 'a'] / 2;
+42int n = count['n' - 'a'];
+43
+44return Math.min(
+45    Math.min(b, a),
+46    Math.min(Math.min(l, o), n)
+47);
+48*/
