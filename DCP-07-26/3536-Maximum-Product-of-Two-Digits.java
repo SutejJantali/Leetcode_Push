89bd6becbb4a052b7a03b84686f692168befe1fb
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-07-26 00:28:48 +0530

    [3536] [Time Beats: 100.00%] [Memory Beats: 92.77%] - LeetPush

--- DCP-07-26/3536-Maximum-Product-of-Two-Digits.java
+++ DCP-07-26/3536-Maximum-Product-of-Two-Digits.java
@@ -1,13 +1,21 @@
 class Solution {
     public int maxProduct(int n) {
-        ArrayList<Integer> res = new ArrayList<>();
+        int first_max = 0, sec_max = 0;
+        // ArrayList<Integer> res = new ArrayList<>();
         while (n > 0){
-            res.add(n%10);
+            if (n%10 > first_max){
+                sec_max = first_max;
+                first_max = n%10;
+            }
+            else if (n%10 > sec_max)
+            sec_max = n%10;
+            // res.add(n%10);
             n /= 10;
         }
 
-        Collections.sort(res);
+        // Collections.sort(res);
 
-        return res.get(res.size()-1)*res.get(res.size()-2);
+        // return res.get(res.size()-1)*res.get(res.size()-2);
+        return first_max*sec_max;
     }
 }
