380958a372a55f3ba108044be56476376d9f1e30
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-08-21 23:42:08 +0530

    [2389] [Time Beats: 93.51%] [Memory Beats: 25.88%] - LeetPush

---
+++ 2389-Longest-Subsequence-With-Limited-Sum.py
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++){
+            nums[i] += nums[i-1];
+        }
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++){
+            int mid = Arrays.binarySearch(nums, queries[i]);
+            res[i] = Math.abs(mid+1);
+        }
+
+        return res;
+    }
+}
