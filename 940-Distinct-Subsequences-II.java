610459de0f7e2b5f0fe5390f82e26ae450038070
SutejJantali <94587355+SutejJantali@users.noreply.github.com>
2026-09-08 17:02:07 +0530

    [940] [Time Beats: 89.90%] [Memory Beats: 72.22%] - LeetPush

---
+++ 940-Distinct-Subsequences-II.java
@@ -0,0 +1,16 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 10**9 + 7
+
+        tot = 0
+        dp = [0] * 26
+
+        for c in s:
+            c = ord(c) - 97
+            new = tot + 1 - dp[c]
+            tot = (tot + new) % MOD
+            dp[c] = (dp[c] + new) % MOD
+
+        return tot 
+
+# https://leetcode.com/problems/distinct-subsequences-ii/solutions/8506448/solution-by-la_castille-zs7g
